561d523fcb0357178f3a213b63c833b0e464d3f1
chenlouis <619189810@qq.com>
2015-10-24 08:41:51 +0800

    update bezier animation,add Timer and TimeTask

--- bezieranimation/src/main/java/me/chenfuduo/MainActivity.java
+++ bezieranimation/src/main/java/me/chenfuduo/MainActivity.java
@@ -2,7 +2,9 @@ package me.chenfuduo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +15,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bezierLayout = (BezierLayout) findViewById(R.id.bezierLayout);
+
+        Timer timer = new Timer();
+        MyTask task = new MyTask();
+        timer.scheduleAtFixedRate(task,350,350);
+
     }
 
-    public void startAnimation(View view) {
+    /*public void startAnimation(View view) {
         bezierLayout.addNewImgs();
+    }*/
+
+    class MyTask extends TimerTask{
+
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    bezierLayout.addNewImgs();
+                }
+            });
+        }
     }
+
 
 }
